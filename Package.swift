8daf1759d56e13bf3b129a2829df940f5daddd3e
David Steinacher <david.steinacher@bitmovin.com>
2022-03-04 09:27:27 +0100

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SimultaneouslyScrollView",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SimultaneouslyScrollView",
+            targets: ["SimultaneouslyScrollView"]
+        )
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "SimultaneouslyScrollView",
+            dependencies: []
+        )
+    ]
+)
