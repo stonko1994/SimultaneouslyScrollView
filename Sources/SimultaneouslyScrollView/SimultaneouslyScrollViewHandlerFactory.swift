2a6cd6d7d63d0ec6f152bd93a6a30d916113fc22
David Steinacher <david.steinacher@bitmovin.com>
2023-05-29 15:52:12 +0200

    prevent using the Package on other platforms than iOS

--- Sources/SimultaneouslyScrollView/SimultaneouslyScrollViewHandlerFactory.swift
+++ Sources/SimultaneouslyScrollView/SimultaneouslyScrollViewHandlerFactory.swift
@@ -1,10 +1,20 @@
 import Foundation
 
 /// Factory class to create `SimultaneouslyScrollViewHandler` instance
+@available(iOS 13, *)
+@available(tvOS, unavailable)
+@available(macOS, unavailable)
 public class SimultaneouslyScrollViewHandlerFactory {
+#if os(iOS)
     /// Creates a new `SimultaneouslyScrollViewHandler` instance
     /// - Returns: A new `SimultaneouslyScrollViewHandler` instance
     public static func create() -> SimultaneouslyScrollViewHandler {
         DefaultSimultaneouslyScrollViewHandler()
     }
+#else
+    public static func create() -> SimultaneouslyScrollViewHandler {
+        // Stub for other platforms
+        fatalError()
+    }
+#endif
 }
